4d2435b3fcf317a8bedac137b027d0ddf9188f70
Joshua Walker <jwalks7@byui.edu>
2026-04-23 20:55:25 -0600

    prep work 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentageString = Console.ReadLine();
+        int percentageInt = int.Parse(percentageString);
+        string letter = "";
+
+        if (percentageInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentageInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentageInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentageInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You have a {letter} in the course.");
+
+        if (percentageInt >= 70)
+        {
+            Console.Write("You passed! Great work!");
+        }
+        else
+        {
+            Console.Write("You failed, better luck next time!");
+        }
     }
 }
