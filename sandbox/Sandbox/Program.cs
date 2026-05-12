0df7cd6e76d49a67596545c346491eaabff33836
Joshua Walker <jwalks7@byui.edu>
2026-05-12 13:47:49 -0600

    sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string fileName = "myFile.txt";
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            outputFile.WriteLine("Collection of jokes? ready");
+            string joke = "How many eye doctors does it take to change a lightbulb? 1... or 2";
+            string joke1 = "Who is the best? Yo mama!";
+            outputFile.WriteLine(joke);
+            outputFile.WriteLine(joke1);
+        }
+
+        LoadFile(fileName);
+
+        static void LoadFile(string fileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split("? ");
+                string joke = parts[0];
+                string answer = parts[1];
+                Console.WriteLine(joke);
+                Console.WriteLine(answer);
+            }
+        }
     }
 }
