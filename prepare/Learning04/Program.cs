3a456690654b12beff1b085502b4a1397cf78d9c
Joshua Walker <jwalks7@byui.edu>
2026-06-01 11:20:14 -0600

    learning week 4 activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment test1 = new MathAssignment("James", "History", "7.3", "8-19");
+        Console.WriteLine(test1.GetSummary());
+        Console.WriteLine(test1.GetHomeworkList());
+        WritingAssignment test2 = new WritingAssignment("Crystal", "English", "Tale of Two Cities");
+        Console.WriteLine(test2.GetSummary());
+        Console.WriteLine(test2.GetWritingInformation());
     }
 }
