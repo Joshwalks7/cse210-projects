ac59783d51361ffac517a2978de495378fc37cf8
Joshua Walker <jwalks7@byui.edu>
2026-06-15 20:34:26 -0600

    learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new List<Shape>();
+        Square square = new Square("black", 4);
+        list.Add(square);
+        Rectangle rectangle = new Rectangle("black", 4, 5);
+        list.Add(rectangle);
+        Circle circle = new Circle("black", 4);
+        list.Add(circle);
+        foreach (Shape s in list)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
+        
     }
 }
